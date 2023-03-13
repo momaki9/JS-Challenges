0d776190e5605078a2031476fecae837a59e0caa
momaki9 <mostafa_m9@yahoo.com>
2023-03-12 21:08:55 -0700

    add section to display and clear game record

--- game/game.js
+++ game/game.js
@@ -1,13 +1,18 @@
 console.log("Rock Paper Scissors Game");
+
 var wins = 0;
+wins = localStorage.getItem('wins');
 var losses = 0;
+losses = localStorage.getItem('losses');
 var ties = 0;
+ties = localStorage.getItem('ties');
+
 var options = ["R", "P", "S"];
 
-var runGame = function() {
+var runGame = function () {
 
     var userChoice = window.prompt('Enter R, P, or S:');
-    
+
     var randomNumber = Math.floor(Math.random() * options.length);
 
     var computerChoice = options[randomNumber];
@@ -50,22 +55,38 @@ var runGame = function() {
     if (playAgain) {
         runGame();
     } else {
+        gameStats();
         return;
     }
-    
+
 };
+
+var gameStats = function () {
+    var winStat = document.getElementById('wins');
+    var storedWinStat = localStorage.getItem('wins');
+    winStat.innerHTML = storedWinStat;
+
+    var lossStat = document.getElementById('losses');
+    var storedlossStat = localStorage.getItem('losses');
+    lossStat.innerHTML = storedlossStat;
+
+    var tieStat = document.getElementById('ties');
+    var storedtieStat = localStorage.getItem('ties');
+    tieStat.innerHTML = storedtieStat;
+};
+
+gameStats();
 
 var startBtn = document.getElementById('start-game');
 startBtn.addEventListener('click', runGame);
 
-var winStat = document.getElementById('wins');
-var storedWinStat = localStorage.getItem('wins');
-winStat.innerHTML = storedWinStat;
+var clearStatBtn = document.getElementById('clear-stats');
 
-var lossStat = document.getElementById('losses');
-var storedlossStat = localStorage.getItem('losses');
-lossStat.innerHTML = storedlossStat;
+var clearStats = function() {
+    localStorage.removeItem("wins");
+    localStorage.removeItem("losses");
+    localStorage.removeItem("ties");
+    window.location.reload();
+};
 
-var tieStat = document.getElementById('ties');
-var storedtieStat = localStorage.getItem('ties');
-tieStat.innerHTML = storedtieStat;
+clearStatBtn.addEventListener("click", clearStats);

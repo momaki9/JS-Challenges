ed232f3f481c77d48eece69e5089dc2eb65f669b
momaki9 <mostafa_m9@yahoo.com>
2023-03-19 20:16:05 -0700

    started working on word guess game

---
+++ Web_APIs/word-guess/script.js
@@ -0,0 +1,32 @@
+var startBtn = document.getElementById('start-btn');
+
+var words = [
+    "JavaScript",
+    "API",
+    "Objects",
+    "Variables",
+    "Button",
+    "HTML",
+    "Stylesheet",
+    "Element",
+    "Array",
+    "String",
+    "Boolean",
+    "JSON",
+    "Expression",
+    "Function",
+    "Loop",
+    "Iterator",
+    "Operator",
+    "Value",
+    "Null",
+    "Undefined"
+];
+
+// write a function that chooses a random word from the words array
+
+// write a function that runs a timer
+
+// write a function that takes the random word and hides it
+
+// write a function that processes the guessed letters from keyboard keys

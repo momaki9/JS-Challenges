f691974cef1b6e6dc19e301e30611b7309077544
momaki9 <mostafa_m9@yahoo.com>
2023-03-20 20:17:18 -0700

    made progress on the word guess game

--- Web_APIs/word-guess/script.js
+++ Web_APIs/word-guess/script.js
@@ -43,7 +43,7 @@ function startTimer() {
     var timeInterval = setInterval(function() {
         time--;
         timeEl.textContent = time;
-        console.log(time);
+        // console.log(time);
         if (time === 0) {
             clearInterval(timeInterval);
         }
@@ -51,22 +51,52 @@ function startTimer() {
 };
 
 // write a function that takes the random word and hides it
+var ouputEl = document.getElementById("interactive");
 
 function guessWord(){
     var chosenWord = chooseRandomWord();
     wordEl.textContent = chosenWord;
-    var letters = chosenWord.split('')
+    var letters = chosenWord.split('');
+    var arrayEl = chosenWord.split('');
     console.log(letters);
     for (let index = 0; index < letters.length; index++) {
+        var childEl = document.createElement('span');
         var blank = "_";
         letters[index] = blank;
+        childEl.textContent = letters[index];
+        childEl.setAttribute("class", "span-element");
+        childEl.setAttribute("aria-valuetext", arrayEl[index])
+        ouputEl.appendChild(childEl);
         console.log(letters[index])
     }
+    // var key = keyEvent();
+    // console.log(key)
+    // var lettersAg = chosenWord.split('');
+    
+    document.addEventListener("keypress", function(event) {
+        var allSpans = document.querySelectorAll('.span-element');
+        // console.log(typeof allSpans)
+        // console.log(allSpans[2].ariaValueText)
+        // console.log(event.key)
+        // console.log(lettersAg)
+        for (let index = 0; index < allSpans.length; index++) {
+            // console.log(allSpans[index].ariaValueText)
+            if (allSpans[index].ariaValueText == event.key) {
+                console.log("good")
+                allSpans[index].textContent = event.key;
+            }
+            
+        }
+
+    });
 }
 
 // write a function that processes the guessed letters from keyboard keys
 function keyEvent(event){
     console.log(event.key);
+    return event.key;
 };
 
-document.addEventListener("keypress", keyEvent);
+// document.addEventListener("keypress", keyEvent);
+
+// make sure to account for capital letters

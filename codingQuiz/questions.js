68c9f4f0015632d30e6677b682b41c0b0f876c19
momaki9 <mostafa_m9@yahoo.com>
2023-07-30 20:12:29 -0700

    implemented timer

--- codingQuiz/questions.js
+++ codingQuiz/questions.js
@@ -6,22 +6,22 @@ var questions = [
     },
     {
         quest: "What does the keyword var mean?",
-        choices: ["variable", "Varsity", "Varying String Size"],
+        choices: ["Variable", "Varsity", "Varying String Size"],
         correct: "Variable"
     },
     {
         quest: "What does the keyword let mean?",
         choices: ["Let is used to declare variables", "Let is the same as var"],
-        correct: "Variable"
+        correct: "Let is used to declare variables"
     },
     {
         quest: "What is a function?",
         choices: ["A variable that does much more than store info", "It calculates the efficiency of the code", "It does a specified task"],
-        correct: "Variable"
+        correct: "It does a specified task"
     },
     {
         quest: "What does DOM mean?",
         choices: ["Dirty Old Man", "Deranged Orphan Mothers", "Document Object Body"],
-        correct: "Variable"
+        correct: "Document Object Body"
     },
 ];
--- codingQuiz/script.js
+++ codingQuiz/script.js
@@ -2,31 +2,87 @@ var startBtn = document.getElementById("quiz-start");
 var question = document.getElementById("quiz-question");
 var questionChoices = document.getElementById("quiz-choices");
 var nextBtnDocument = document.getElementById("next-btn");
+var quizEl = document.getElementById("timer");
 var questionNumber = 0;
+var time = 45;
 
-var currentQuestion= function (questionNumber) {
-    startBtn.hidden = true;
-    question.textContent = questions[questionNumber].quest;
-    for (let index = 0; index < questions[questionNumber].choices.length; index++) {
-        var answerChoice = document.createElement("li");
-        answerChoice.textContent = questions[questionNumber].choices[index];
-        questionChoices.appendChild(answerChoice)
+var currentQuestion = function (questionNumber) {
+    if (questionNumber < questions.length) {
+        startBtn.hidden = true;
+        question.textContent = questions[questionNumber].quest;
+        var correctAnswer = questions[questionNumber].correct;
+        for (let index = 0; index < questions[questionNumber].choices.length; index++) {
+            var answerChoice = document.createElement("button");
+            answerChoice.setAttribute("class", "answer-choices");
+            answerChoice.textContent = questions[questionNumber].choices[index];
+            questionChoices.appendChild(answerChoice);
+            answerChoice.addEventListener("click", function (event) {
+                // console.log(correctAnswer);
+                var chosenAnswer = event.target.textContent;
+                // console.log(chosenAnswer);
+                // console.log(questionNumber);
+                if (chosenAnswer === correctAnswer) {
+                    console.log("correct");
+                    nextQuestion();
+                } else {
+                    console.log("wrong");
+                    time = time - 5;
+                    nextQuestion();
+                }
+            });
+        };
+
+        var nextBtn = document.createElement("button");
+        nextBtn.textContent = "Next";
+        nextBtnDocument.appendChild(nextBtn);
+        if (questionNumber === 4) {
+            nextBtn.hidden = true;
+        }
+        nextBtn.addEventListener("click", nextQuestion);
+    } else {
+        finalPage();
     }
-    var nextBtn = document.createElement("button");
-    nextBtn.textContent = "Next"
-    nextBtnDocument.appendChild(nextBtn);
-    nextBtn.addEventListener("click", nextQuestion)
 };
 
 var nextQuestion = function () {
-    questionChoices.replaceChildren();
-    nextBtnDocument.replaceChildren();
-    startQuiz();
+
+    if (questionNumber < questions.length) {
+        questionChoices.replaceChildren();
+        nextBtnDocument.replaceChildren();
+        questionNumber++;
+        currentQuestion(questionNumber);
+    }
+
 };
 
-var startQuiz = function() {
+var startQuiz = function () {
     currentQuestion(questionNumber);
-    questionNumber++;
-}
+    quizTimer();
+};
+
+var quizTimer = function () {
+    var quizTime = setInterval(function () {
+        if (questionNumber === questions.length) {
+            clearInterval(quizTime);
+        }
+        if (time == 0) {
+            clearInterval(quizTime)
+        } else {
+            time--;
+            quizEl.textContent = time;
+        }
+    }, 1000);
+};
 
 startBtn.addEventListener("click", startQuiz);
+
+var finalPage = function() {
+    questionChoices.replaceChildren();
+    nextBtnDocument.replaceChildren();
+    question.textContent = "Enter Initials to save your score!";
+    var initialsEl = document.createElement("input");
+    var saveInitials = document.createElement("button");
+    saveInitials.textContent = "Save"
+    question.appendChild(initialsEl);
+    question.appendChild(saveInitials);
+};

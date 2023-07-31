5d078cdbe233877b079b2e3712f3d8acebabaa80
momaki9 <mostafa_m9@yahoo.com>
2023-07-30 18:33:49 -0700

    refactored the code to be more efficient

---
+++ codingQuiz/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        quest: "What does JS stand for?",
+        choices: ["Jane Smith - a famous coder", "JavaScript", "A famous computer brand"],
+        correct: "JavaScript"
+    },
+    {
+        quest: "What does the keyword var mean?",
+        choices: ["variable", "Varsity", "Varying String Size"],
+        correct: "Variable"
+    },
+    {
+        quest: "What does the keyword let mean?",
+        choices: ["Let is used to declare variables", "Let is the same as var"],
+        correct: "Variable"
+    },
+    {
+        quest: "What is a function?",
+        choices: ["A variable that does much more than store info", "It calculates the efficiency of the code", "It does a specified task"],
+        correct: "Variable"
+    },
+    {
+        quest: "What does DOM mean?",
+        choices: ["Dirty Old Man", "Deranged Orphan Mothers", "Document Object Body"],
+        correct: "Variable"
+    },
+];
--- codingQuiz/script.js
+++ codingQuiz/script.js
@@ -1,57 +1,32 @@
 var startBtn = document.getElementById("quiz-start");
 var question = document.getElementById("quiz-question");
 var questionChoices = document.getElementById("quiz-choices");
-var questions = [
-    {
-      quest: "What does JS stand for?" ,
-      choices: ["Jane Smith - a famous coder", "JavaScript", "A famous computer brand"],
-      correct: "JavaScript"
-    },
-    {
-        quest: "What does the keyword var mean?" ,
-        choices: ["variable", "Varsity", "Varying String Size"],
-        correct: "Variable"
-    }
-];
+var nextBtnDocument = document.getElementById("next-btn");
+var questionNumber = 0;
 
-var quizContent = document.getElementById("quiz-content");
-
-document.replaceChildren
-
-function questionOne() {
-    console.log("I work");
+var currentQuestion= function (questionNumber) {
     startBtn.hidden = true;
-    // var question = document.createElement("h3");
-    question.textContent = questions[0].quest;
-    // quizContent.appendChild(question);
-    for (let index = 0; index < questions[0].choices.length; index++) {
+    question.textContent = questions[questionNumber].quest;
+    for (let index = 0; index < questions[questionNumber].choices.length; index++) {
         var answerChoice = document.createElement("li");
-        answerChoice.setAttribute("id", "testing123")
-        answerChoice.textContent = questions[0].choices[index];
+        answerChoice.textContent = questions[questionNumber].choices[index];
         questionChoices.appendChild(answerChoice)
     }
     var nextBtn = document.createElement("button");
     nextBtn.textContent = "Next"
-    quizContent.appendChild(nextBtn);
-    nextBtn.addEventListener("click", emptyChoices)
+    nextBtnDocument.appendChild(nextBtn);
+    nextBtn.addEventListener("click", nextQuestion)
 };
 
-var emptyChoices = function(){
-    console.log(questionChoices.children);
+var nextQuestion = function () {
     questionChoices.replaceChildren();
-    questionTwo();
+    nextBtnDocument.replaceChildren();
+    startQuiz();
 };
 
-function questionTwo() {
-    console.log("I work too");
-    // var question = document.createElement("h3");
-    question.textContent = questions[1].quest;
-    // quizContent.appendChild(question);
-    for (let index = 0; index < questions[1].choices.length; index++) {
-        var answerChoice = document.createElement("li");
-        answerChoice.textContent = questions[1].choices[index];
-        questionChoices.appendChild(answerChoice)
-    }  
-};
+var startQuiz = function() {
+    currentQuestion(questionNumber);
+    questionNumber++;
+}
 
-startBtn.addEventListener("click", questionOne);
+startBtn.addEventListener("click", startQuiz);
